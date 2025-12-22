f6a99cdd8169e79140adafc209962aac3c79a75a
mokaradag <mokaradag@gmail.com>
2025-12-22 12:18:00 +0300

    rollback

--- www/js/cinematic_video.js
+++ www/js/cinematic_video.js
@@ -249,16 +249,12 @@ const CinematicVideoManager = {
         this.playSequence('select');
     },
 
-	stopEverything: function() {
+    stopEverything: function() {
         this.clearTimer();
         this.pauseVideo();
         this.showImage();
         this.state.isPlaying = false;
         this.state.lastVideoType = null;
-        
-        if (typeof CharacterBorderEffects !== 'undefined') {
-            CharacterBorderEffects.destroy();
-        }
     },
 
     clearTimer: function() {
