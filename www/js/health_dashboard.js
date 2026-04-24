95995408755798927c1cb9c6ffe7eb521281882a
mokaradag <mokaradag@gmail.com>
2026-04-24 18:45:28 +0300

    Initialize health dashboard tooltips

--- www/js/health_dashboard.js
+++ www/js/health_dashboard.js
@@ -13,7 +13,29 @@
     }
   }
 
+  function initHealthTooltips() {
+    if (!window.jQuery || !jQuery.fn || !jQuery.fn.tooltip) {
+      return;
+    }
+
+    jQuery(".health-dashboard-container [data-toggle='tooltip']").tooltip("dispose").tooltip({
+      container: "body",
+      trigger: "hover",
+      delay: { show: 120, hide: 250 },
+      placement: function(tip, element) {
+        return jQuery(element).attr("data-placement") || "top";
+      }
+    });
+  }
+
   if (window.Shiny && Shiny.addCustomMessageHandler) {
     Shiny.addCustomMessageHandler("updateHealthTimestamp", updateHealthTimestamp);
+    Shiny.addCustomMessageHandler("initHealthTooltips", function() {
+      window.setTimeout(initHealthTooltips, 80);
+    });
   }
+
+  document.addEventListener("DOMContentLoaded", function() {
+    window.setTimeout(initHealthTooltips, 250);
+  });
 })();
