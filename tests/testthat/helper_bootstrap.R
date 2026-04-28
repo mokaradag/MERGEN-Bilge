5a60ce548ff3399d1c69b575b9119c68b6d986fe
mokaradag <mokaradag@gmail.com>
2026-04-28 09:29:59 +0300

    Load file manager policy helpers in tests

--- tests/testthat/helper_bootstrap.R
+++ tests/testthat/helper_bootstrap.R
@@ -116,4 +116,5 @@ source(file.path(repo_root_for_tests, "R", "helpers_db_validation.R"), encoding 
 source(file.path(repo_root_for_tests, "R", "helpers_chat_message_formatting.R"), encoding = "UTF-8", local = .test_global)
 source(file.path(repo_root_for_tests, "R", "helpers_database.R"), encoding = "UTF-8", local = .test_global)
 source(file.path(repo_root_for_tests, "R", "utils_file_index.R"), encoding = "UTF-8", local = .test_global)
+source(file.path(repo_root_for_tests, "R", "helpers_file_manager_policy.R"), encoding = "UTF-8", local = .test_global)
 source(file.path(repo_root_for_tests, "R", "utils_rate_limiter.R"), encoding = "UTF-8", local = .test_global)
