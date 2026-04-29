8ffa26068faa2035e5108d0fc99bafb3429cf7a0
mokaradag <mokaradag@gmail.com>
2026-04-29 12:49:35 +0300

    tüm testler başarılı

--- tests/testthat/test-maintainability-ratchet.R
+++ tests/testthat/test-maintainability-ratchet.R
@@ -56,7 +56,7 @@ test_that("maintainability skoru mevcut taban çizgisinin altına düşmez", {
     info = "maintainability_report.R attr(..., 'maintainability_score') üretmelidir."
   )
 
-  min_score <- .as_int_env("MERGEN_TEST_MIN_MAINTAINABILITY_SCORE", 29L)
+  min_score <- .as_int_env("MERGEN_TEST_MIN_MAINTAINABILITY_SCORE", 34L)
 
   expect_true(
     score >= min_score,
@@ -90,9 +90,9 @@ test_that("büyük dosya ve fonksiyon sayaçları mevcut taban çizgisinden köt
 
   max_large_files <- .as_int_env("MERGEN_TEST_MAX_800_LINE_FILES", 14L)
   max_function_heavy_files <- .as_int_env("MERGEN_TEST_MAX_25_FUNCTION_FILES", 12L)
-  max_very_large_files <- .as_int_env("MERGEN_TEST_MAX_1500_LINE_FILES", 1L)
-  max_file_lines <- .as_int_env("MERGEN_TEST_MAX_FILE_LINES", 2366L)
-  max_file_functions <- .as_int_env("MERGEN_TEST_MAX_FILE_FUNCTIONS", 96L)
+  max_very_large_files <- .as_int_env("MERGEN_TEST_MAX_1500_LINE_FILES", 0L)
+  max_file_lines <- .as_int_env("MERGEN_TEST_MAX_FILE_LINES", 1468L)
+  max_file_functions <- .as_int_env("MERGEN_TEST_MAX_FILE_FUNCTIONS", 52L)
 
   actual_large_files <- sum(score_report$lines >= 800)
   actual_function_heavy_files <- sum(score_report$functions >= 25)
