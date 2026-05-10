87d1d6b221b17c933d4cfe0ba7af8a61fa9e71b0
mokaradag <mokaradag@gmail.com>
2026-05-10 17:23:39 +0300

    testthat fails

--- tests/testthat/test-frontend-selector-contract.R
+++ tests/testthat/test-frontend-selector-contract.R
@@ -6,7 +6,40 @@
 
 .read_repo_text_frontend_selector_contract <- function(path) {
   full_path <- file.path(resolve_repo_root_for_tests(), path)
-  paste(readLines(full_path, warn = FALSE, encoding = "UTF-8"), collapse = "\n")
+
+  size <- suppressWarnings(file.info(full_path)$size[1])
+  if (is.na(size) || size <= 0) {
+    return("")
+  }
+
+  con <- file(full_path, open = "rb")
+  on.exit(close(con), add = TRUE)
+
+  raw_data <- readBin(con, what = "raw", n = size)
+
+  txt <- suppressWarnings(
+    iconv(list(raw_data), from = "UTF-8", to = "UTF-8", sub = "byte")[[1]]
+  )
+
+  if (is.na(txt)) {
+    txt <- ""
+  }
+
+  txt <- gsub("\\r\\n?|\\r", "\n", txt, perl = TRUE)
+  enc2utf8(txt)
+}
+
+.frontend_selector_has_text <- function(text, needle) {
+  isTRUE(suppressWarnings(grepl(
+    needle,
+    text,
+    fixed = TRUE,
+    useBytes = TRUE
+  )))
+}
+
+.frontend_selector_lacks_text <- function(text, needle) {
+  !.frontend_selector_has_text(text, needle)
 }
 
 test_that("ön yüz seçici sözleşmeleri güncel UI ile hizalı kalır", {
@@ -21,42 +54,42 @@ test_that("ön yüz seçici sözleşmeleri güncel UI ile hizalı kalır", {
   claude_js <- .read_repo_text_frontend_selector_contract("www/js/claude_code.js")
   claude_streaming_js <- .read_repo_text_frontend_selector_contract("www/js/claude_code_streaming.js")
 
-  expect_true(grepl('id = "user_input"', ui_text, fixed = TRUE))
-  expect_false(grepl("message_input", input_js, fixed = TRUE))
-  expect_true(grepl("#user_input, .chat-input", input_js, fixed = TRUE))
-  expect_true(grepl('inputId = "send_stop_btn"', ui_text, fixed = TRUE))
-  expect_true(grepl("#send_stop_btn", input_js, fixed = TRUE))
+  expect_true(.frontend_selector_has_text(ui_text, 'id = "user_input"'))
+  expect_true(.frontend_selector_lacks_text(input_js, "message_input"))
+  expect_true(.frontend_selector_has_text(input_js, "#user_input, .chat-input"))
+  expect_true(.frontend_selector_has_text(ui_text, 'inputId = "send_stop_btn"'))
+  expect_true(.frontend_selector_has_text(input_js, "#send_stop_btn"))
 
-  expect_true(grepl('id = "welcome_fullscreen_container"', ui_text, fixed = TRUE))
-  expect_true(grepl("#welcome_fullscreen_container", shiny_handlers_js, fixed = TRUE))
-  expect_true(grepl('id = "chat_content_container"', ui_text, fixed = TRUE))
+  expect_true(.frontend_selector_has_text(ui_text, 'id = "welcome_fullscreen_container"'))
+  expect_true(.frontend_selector_has_text(shiny_handlers_js, "#welcome_fullscreen_container"))
+  expect_true(.frontend_selector_has_text(ui_text, 'id = "chat_content_container"'))
 
-  expect_true(grepl('id = "chat_input_wrapper"', ui_text, fixed = TRUE))
-  expect_true(grepl("#chat_input_wrapper", file_handlers_js, fixed = TRUE))
-  expect_true(grepl('id = "drop_zone"', ui_text, fixed = TRUE))
-  expect_true(grepl("#drop_zone", file_handlers_js, fixed = TRUE))
-  expect_true(grepl('fileInput("file_upload"', ui_text, fixed = TRUE))
-  expect_true(grepl("document.getElementById('file_upload')", file_handlers_js, fixed = TRUE))
-  expect_true(grepl('id = "file_btn_container"', ui_text, fixed = TRUE))
-  expect_true(grepl("#file_btn_container", file_handlers_js, fixed = TRUE))
+  expect_true(.frontend_selector_has_text(ui_text, 'id = "chat_input_wrapper"'))
+  expect_true(.frontend_selector_has_text(file_handlers_js, "#chat_input_wrapper"))
+  expect_true(.frontend_selector_has_text(ui_text, 'id = "drop_zone"'))
+  expect_true(.frontend_selector_has_text(file_handlers_js, "#drop_zone"))
+  expect_true(.frontend_selector_has_text(ui_text, 'fileInput("file_upload"'))
+  expect_true(.frontend_selector_has_text(file_handlers_js, "document.getElementById('file_upload')"))
+  expect_true(.frontend_selector_has_text(ui_text, 'id = "file_btn_container"'))
+  expect_true(.frontend_selector_has_text(file_handlers_js, "#file_btn_container"))
 
-  expect_true(grepl('class = "attach-checkbox"', file_manager_table_r, fixed = TRUE))
-  expect_true(grepl("input$attach_toggled", file_manager_module_r, fixed = TRUE))
-  expect_true(grepl("change.attach", file_manager_module_r, fixed = TRUE))
-  expect_true(grepl("drawCallback", file_manager_module_r, fixed = TRUE))
-  expect_true(grepl("input.attach-checkbox[data-filename]", shiny_handlers_js, fixed = TRUE))
+  expect_true(.frontend_selector_has_text(file_manager_table_r, 'class = "attach-checkbox"'))
+  expect_true(.frontend_selector_has_text(file_manager_module_r, "input$attach_toggled"))
+  expect_true(.frontend_selector_has_text(file_manager_module_r, "change.attach"))
+  expect_true(.frontend_selector_has_text(file_manager_module_r, "drawCallback"))
+  expect_true(.frontend_selector_has_text(shiny_handlers_js, "input.attach-checkbox[data-filename]"))
 
-  expect_true(grepl('canvasId = ns("visualizer_canvas")', stt_module_r, fixed = TRUE))
-  expect_true(grepl('timerId = ns("stt_timer")', stt_module_r, fixed = TRUE))
-  expect_true(grepl('dbId = ns("stt_db_indicator")', stt_module_r, fixed = TRUE))
+  expect_true(.frontend_selector_has_text(stt_module_r, 'canvasId = ns("visualizer_canvas")'))
+  expect_true(.frontend_selector_has_text(stt_module_r, 'timerId = ns("stt_timer")'))
+  expect_true(.frontend_selector_has_text(stt_module_r, 'dbId = ns("stt_db_indicator")'))
 
-  expect_true(grepl('id = ns("prompt_input")', claude_ui_r, fixed = TRUE))
-  expect_true(grepl("prompt_input", claude_js, fixed = TRUE))
-  expect_true(grepl('id = ns("output_area")', claude_ui_r, fixed = TRUE))
-  expect_true(grepl("cc-stream-chunk", claude_streaming_js, fixed = TRUE))
-  expect_true(grepl("document.getElementById(data.target)", claude_streaming_js, fixed = TRUE))
-  expect_true(grepl('id = ns("welcome_screen")', claude_ui_r, fixed = TRUE))
-  expect_true(grepl("data.welcomeId", claude_streaming_js, fixed = TRUE))
-  expect_true(grepl('ns("run_command")', claude_ui_r, fixed = TRUE))
-  expect_true(grepl('ns("stop_command")', claude_ui_r, fixed = TRUE))
+  expect_true(.frontend_selector_has_text(claude_ui_r, 'id = ns("prompt_input")'))
+  expect_true(.frontend_selector_has_text(claude_js, "prompt_input"))
+  expect_true(.frontend_selector_has_text(claude_ui_r, 'id = ns("output_area")'))
+  expect_true(.frontend_selector_has_text(claude_streaming_js, "cc-stream-chunk"))
+  expect_true(.frontend_selector_has_text(claude_streaming_js, "document.getElementById(data.target)"))
+  expect_true(.frontend_selector_has_text(claude_ui_r, 'id = ns("welcome_screen")'))
+  expect_true(.frontend_selector_has_text(claude_streaming_js, "data.welcomeId"))
+  expect_true(.frontend_selector_has_text(claude_ui_r, 'ns("run_command")'))
+  expect_true(.frontend_selector_has_text(claude_ui_r, 'ns("stop_command")'))
 })
