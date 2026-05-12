357ec869c2d4bd8b3923fc8b7ddedd9dfea736c6
mokaradag <mokaradag@gmail.com>
2026-05-12 16:48:19 +0300

    All pass

--- tests/testthat/helper_bootstrap.R
+++ tests/testthat/helper_bootstrap.R
@@ -110,6 +110,7 @@ repo_root_for_tests <- resolve_repo_root_for_tests()
 
 # Testlerde kullanılan yardımcı fonksiyon ve dosyaları global ortama yükler.
 source(file.path(repo_root_for_tests, "R", "utils_safe_source.R"), encoding = "UTF-8", local = .test_global)
+source(file.path(repo_root_for_tests, "R", "utils_text_encoding.R"), encoding = "UTF-8", local = .test_global)
 source(file.path(repo_root_for_tests, "R", "helpers_worker_monitor.R"), encoding = "UTF-8", local = .test_global)
 source(file.path(repo_root_for_tests, "R", "helpers_db_connection.R"), encoding = "UTF-8", local = .test_global)
 source(file.path(repo_root_for_tests, "R", "helpers_db_validation.R"), encoding = "UTF-8", local = .test_global)
