6222f34e366dbe1771221f1949bb25d1b3d76137
mokaradag <mokaradag@gmail.com>
2026-05-13 16:07:39 +0300

    İyileştirme

--- R/server_runtime_context.R
+++ R/server_runtime_context.R
@@ -5,34 +5,20 @@
 #           yayılımını azaltmak ve eksik/yanlış init sırasını erken yakalamaktır.
 # ==============================================================================
 
-.server_runtime_contract_sources <- file.path("R", c(
-  "helpers_server_runtime_contracts.R",
-  "helpers_server_runtime_named_contracts.R"
-))
-
+.server_runtime_contract_sources <- file.path("R", c("helpers_server_runtime_contracts.R", "helpers_server_runtime_named_contracts.R"))
 .server_runtime_contract_helpers <- c(
   "is_server_runtime_context", ".server_runtime_stop",
   ".server_runtime_require_context", ".server_runtime_require_values",
   ".server_runtime_require_functions", ".server_runtime_require_named_functions",
   ".server_runtime_require_environment", ".server_runtime_invoke_auth_ready_callback"
 )
-
-.server_runtime_missing_contract_helpers <- .server_runtime_contract_helpers[!vapply(
-  .server_runtime_contract_helpers,
-  function(fn_name) exists(fn_name, mode = "function", inherits = TRUE),
-  logical(1)
-)]
+.server_runtime_missing_contract_helpers <- .server_runtime_contract_helpers[!vapply(.server_runtime_contract_helpers, exists, logical(1), mode = "function", inherits = TRUE)]
 
 if (length(.server_runtime_missing_contract_helpers) > 0L) {
   for (.contract_source in .server_runtime_contract_sources[file.exists(.server_runtime_contract_sources)]) {
     source(.contract_source, encoding = "UTF-8", local = globalenv())
   }
-
-  .server_runtime_missing_contract_helpers <- .server_runtime_contract_helpers[!vapply(
-    .server_runtime_contract_helpers,
-    function(fn_name) exists(fn_name, mode = "function", inherits = TRUE),
-    logical(1)
-  )]
+  .server_runtime_missing_contract_helpers <- .server_runtime_contract_helpers[!vapply(.server_runtime_contract_helpers, exists, logical(1), mode = "function", inherits = TRUE)]
 }
 
 if (length(.server_runtime_missing_contract_helpers) > 0L) {
@@ -42,11 +28,10 @@ if (length(.server_runtime_missing_contract_helpers) > 0L) {
   ), call. = FALSE)
 }
 
-rm(list = intersect(
-  c(".server_runtime_contract_sources", ".server_runtime_contract_helpers",
-    ".server_runtime_missing_contract_helpers", ".contract_source"),
-  ls()
-))
+rm(list = intersect(c(
+  ".server_runtime_contract_sources", ".server_runtime_contract_helpers",
+  ".server_runtime_missing_contract_helpers", ".contract_source"
+), ls()))
 
 serverRuntimeRequireIdentity <- function(ctx,
                                          required_values = character(0),
