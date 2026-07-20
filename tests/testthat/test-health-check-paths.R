a9c5a4e5ac041ef565d8e62ea37b750209c5f934
mokaradag <mokaradag@gmail.com>
2026-07-20 21:06:21 +0300

    test: UNC yanlış negatif sağlık kontrolünü kilitle

--- tests/testthat/test-health-check-paths.R
+++ tests/testthat/test-health-check-paths.R
@@ -54,6 +54,55 @@ test_that("henüz oluşmamış index JSON yazılabilir üst klasörde kritik de�
   expect_false(file.exists(idx))
 })
 
+test_that("UNC varlık sorguları yanlış negatif olsa da gerçek yazma kanıttır", {
+  tmp <- tempfile("unc-health-probe-")
+  dir.create(tmp, recursive = TRUE)
+  idx <- file.path(tmp, "index.json")
+
+  had_dir_exists <- exists("dir.exists", envir = .GlobalEnv, inherits = FALSE)
+  old_dir_exists <- if (had_dir_exists) get("dir.exists", envir = .GlobalEnv, inherits = FALSE) else NULL
+  had_relaxed <- exists("path_exists_relaxed", envir = .GlobalEnv, inherits = FALSE)
+  old_relaxed <- if (had_relaxed) get("path_exists_relaxed", envir = .GlobalEnv, inherits = FALSE) else NULL
+
+  withr::defer({
+    if (had_dir_exists) assign("dir.exists", old_dir_exists, envir = .GlobalEnv)
+    else if (exists("dir.exists", envir = .GlobalEnv, inherits = FALSE)) rm(list = "dir.exists", envir = .GlobalEnv)
+
+    if (had_relaxed) assign("path_exists_relaxed", old_relaxed, envir = .GlobalEnv)
+    else if (exists("path_exists_relaxed", envir = .GlobalEnv, inherits = FALSE)) rm(list = "path_exists_relaxed", envir = .GlobalEnv)
+  })
+
+  assign("dir.exists", function(...) FALSE, envir = .GlobalEnv)
+  assign("path_exists_relaxed", function(...) FALSE, envir = .GlobalEnv)
+
+  root_res <- health_check_path_writable("unc.write", "UNC Yazma", tmp)
+  index_res <- health_check_index_json(idx)
+
+  expect_identical(root_res$status[1], "ok")
+  expect_identical(index_res$status[1], "ok")
+  expect_false(file.exists(idx))
+})
+
+test_that("index kontrolü ham ortam değeri yerine kanonik runtime yolunu kullanır", {
+  tmp <- tempfile("index-runtime-root-")
+  dir.create(tmp, recursive = TRUE)
+  idx <- file.path(tmp, "index.json")
+  invalid_idx <- file.path(tmp, "missing-parent", "index.json")
+
+  old_option <- options(mergen.index_path = idx)
+  old_env <- Sys.getenv("MERGEN_INDEX_PATH", unset = NA_character_)
+  withr::defer({
+    options(old_option)
+    if (is.na(old_env)) Sys.unsetenv("MERGEN_INDEX_PATH") else Sys.setenv(MERGEN_INDEX_PATH = old_env)
+  })
+  Sys.setenv(MERGEN_INDEX_PATH = invalid_idx)
+
+  res <- health_check_index_json()
+
+  expect_identical(res$status[1], "ok")
+  expect_identical(res$value[1], idx)
+})
+
 test_that("boş index yolu tanımlı değil olarak döner", {
   res <- health_check_index_json("")
   expect_identical(res$status[1], "not_configured")
