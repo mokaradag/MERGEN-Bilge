fe6e8e8964657f6ac7b06205fbdbc22aeea95e2a
mokaradag <mokaradag@gmail.com>
2026-07-29 09:33:03 +0300

    test: guard shared-session light UI contract

---
+++ tests/testthat/test-ortak-oturum-light-ui-contract.R
@@ -0,0 +1,24 @@
+# Ortak oturum açık tema kontrastı ve modal yerleşimi sözleşmesi.
+
+.oo_light_css_oku <- function(yol) {
+  baytlar <- readBin(yol, what = "raw", n = file.info(yol)$size)
+  iconv(rawToChar(baytlar), from = "UTF-8", to = "UTF-8", sub = "byte")
+}
+
+test_that("ortak oturum açık tema kontrastı ve modal yerleşimi korunur", {
+  repo_root <- resolve_repo_root_for_tests()
+  css <- .oo_light_css_oku(file.path(repo_root, "www", "css", "ortak_oturumlar_light.css"))
+
+  expect_true(grepl(".oo-hub-filtreler .radio-inline:has", css, fixed = TRUE))
+  expect_true(grepl(".oo-davet-paneli .radio-inline:has", css, fixed = TRUE))
+  expect_true(grepl("background: #1d4ed8 !important", css, fixed = TRUE))
+  expect_true(grepl("color: #ffffff !important", css, fixed = TRUE))
+
+  expect_true(grepl(".modal-dialog:has(.oo-yeni-oturum-modal)", css, fixed = TRUE))
+  expect_true(grepl("max-width: 720px", css, fixed = TRUE))
+  expect_true(grepl("grid-template-columns: 1fr", css, fixed = TRUE))
+  expect_true(grepl("white-space: nowrap", css, fixed = TRUE))
+
+  expect_true(grepl(".oo-secici .btn i", css, fixed = TRUE))
+  expect_true(grepl("color: #334155 !important", css, fixed = TRUE))
+})
